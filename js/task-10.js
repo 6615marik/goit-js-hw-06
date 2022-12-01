cdf8afae9a3fa6579499f53dd9f87606a64c6f5d
Yen Maryna <113536885+6615marik@users.noreply.github.com>
2022-12-01 22:25:16 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const ref = {
+  boxMein: document.querySelector('#boxes'),
+  input: document.querySelector('input'),
+  btnCreate: document.querySelector('[data-create]'),
+  btnDestroy:document.querySelector('[data-destroy]'),
+}
+// console.log(ref.btnDestroy);
+
+ref.btnCreate.addEventListener('click',createBoxes);
+ref.btnDestroy.addEventListener('click',destroyBoxes);
+
+function createBoxes() {
+  const n = ref.input.value;
+  const items = [];
+
+  for (let i = 1; i <= n; i += 1) {
+  const createDiv = document.createElement("div");
+
+    createDiv.style.backgroundColor = getRandomHexColor();
+    createDiv.style.width = i + 10+ "px";
+    createDiv.style.height = i + 10 + "px";
+
+    items.push(createDiv);
+}
+ref.boxMein.append(...items);
+}
+
+function destroyBoxes () {
+ref.boxMein.innerHTML = "";
+};
